538e26c697be4ea12b18bc2a00969b6c43e71732
jtran81124 <43736400+jtran81124@users.noreply.github.com>
2019-12-04 14:10:50 -0800

    reupdate

---
+++ routing/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Nav from './Nav';
+import About from './About';
+import Shop from './Shop';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    //put all your components in here
+    <Router>
+      <div className="App">
+        <h1> wassap </h1>
+        <Nav/>
+        <Switch>
+           {/*hits only one of these pages, not all with a switch from top to botom*/}
+          {/* but using exact, the path must be exact to show the component */}
+          <Route  path="/" exact component ={Home} />
+          <Route path="/about" component ={About} />
+          <Route path="/shop" component ={Shop} />
+        </Switch>
+        
+
+      </div>
+    </Router>
+    
+  );
+}
+
+//home component
+const Home = () => (
+  <div>
+    <h1> Home Page </h1>
+  </div>
+)
+
+export default App;
