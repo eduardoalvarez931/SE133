196efdc9c19bbc498225ca423684ab08ef0057ce
jtran81124 <43736400+jtran81124@users.noreply.github.com>
2019-12-03 17:43:10 -0800

    trying something

---
+++ learn-react/chords/src/components/NewPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This should only show a new page</h1>
+      </div>
+    );
+  }
+}
+
+export default NewPage;
